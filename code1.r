9dadfb3686567b75a2aee601126c36f9290397fa
Payne-Wang <44337721+Payne-Wang@users.noreply.github.com>
2024-11-15 10:29:37 +0800

    Update code1.r

--- code1.r
+++ code1.r
@@ -4,6 +4,7 @@ library(lubridate)
 library(logging)
 library(base64enc)
 library(dplyr)
+library(stringr)
 
 #使用代理请求
 Sys.setenv(http_proxy="http://127.0.0.1:7890")
@@ -33,7 +34,7 @@ at<-content(resp)$data$access_token
 
 #通过at获取渠道信息
 resp<-GET(
-  url=modify_url(url,path = "/api/channel/?p=0&page_size=1000"),  #这里默认只检测第一页，可以写个判断，让其检测所有页
+  url=modify_url(url,path = "/api/channel/?p=0&page_size=1000&id_sort=true"),  #这里默认只检测第一页，可以写个判断，让其检测所有页
   add_headers(Authorization=paste("Bearer",at),"New-Api-User"="1")
 )
 
@@ -49,8 +50,65 @@ lapply(cha_id,function(x){
   )
   cha_df<-content(resp_1)$data
   
-})
+})%>%sapply(., function(x){c(x$id,x$models)},simplify = T)%>%
+  
+  as.matrix()%>%t()%>%as.data.frame()%>%setNames(c("id","models"))->apikey_channelid
 
+#只验证其中包含gpt-4o,o1-mini,o1-preview,claude-3-5-sonnet-20240620,claude-3-5-sonnet-20241022的通道是否可用
+interested_models<-c("gpt-4o","chatgpt-4o-latest","gpt-4o-2024-08-06","gpt-4o-2024-05-13",
+                     "o1-mini","o1-preview",
+                     "claude-3-5-sonnet-20240620","claude-3-5-sonnet-20241022")%>%{
+                       type1=paste0(.,",")%>%paste0(.,collapse = "|")
+                       type2=paste0(.,"$")%>%paste0(.,collapse = "|")
+                       paste(type1,type2,sep = "|")
+                     }
+apikey_channelid%>%
+  filter(models%>%str_detect(.,interested_models))%>%
+  mutate(models=str_extract_all(models,interested_models)%>%sapply(.,function(x){paste0(x,collapse = "")}))%>%{
+    sapply(1:nrow(.),function(x){
+      models=str_split(.[x,"models"],",",simplify = T)%>%as.character%>%.[.!=""]
+      id=.[x,"id"]
+      sapply(models,function(y){
+        tryCatch({
+          resp<-GET(
+            url=modify_url(url,path = paste0("/api/channel/test/",id,"?model=",y)),
+            add_headers(Authorization=paste("Bearer",at),"New-Api-User"="1"),
+            timeout(20)
+          )
+          content(resp)$success
+        },error=function(e){return("FALSE")})
+      })%>%paste(.,collapse = ",")
+    })->res
+    
+    data.frame(id=.$id,models=.$models,response=res)
+  }
+
+#只对openai的渠道进行测活以便将获取的新的key加上
+interested_model_2<-c("gpt-4o","o1-mini","o1-preview")%>%{
+                       type1=paste0(.,",")%>%paste0(.,collapse = "|")
+                       type2=paste0(.,"$")%>%paste0(.,collapse = "|")
+                       paste(type1,type2,sep = "|")
+                     }
+apikey_channelid%>%
+  filter(models%>%str_detect(.,interested_model_2))%>%
+  mutate(models=str_extract_all(models,interested_model_2)%>%sapply(.,function(x){paste0(x,collapse = "")}))%>%{
+    sapply(1:nrow(.),function(x){
+      models=str_split(.[x,"models"],",",simplify = T)%>%as.character%>%.[.!=""]
+      id=.[x,"id"]
+      sapply(models,function(y){
+        tryCatch({
+          resp<-GET(
+            url=modify_url(url,path = paste0("/api/channel/test/",id,"?model=",y)),
+            add_headers(Authorization=paste("Bearer",at),"New-Api-User"="1"),
+            timeout(20)
+          )
+          content(resp)$success
+        },error=function(e){return("FALSE")})
+      })%>%paste(.,collapse = ",")
+    })->res
+    
+    data.frame(id=.$id,models=.$models,response=res)
+  }
 
 # #通过at获取每个渠道的key,并浅测一下key
 # lapply(cha_id,function(x){
